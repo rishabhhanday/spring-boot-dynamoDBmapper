c164510b379d3693a8359a3becc072bbaecbba8f
Rishabh Handay <rishabhhanday.rh@gmail.com>
2020-08-31 18:13:21 +0530

    init commit

---
+++ src/main/java/com/poc/dynamoDbMapper/DynamoDbMapperApplication.java
@@ -0,0 +1,13 @@
+package com.poc.dynamoDbMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/dynamoDbMapper/DynamoDbMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.dynamoDbMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
